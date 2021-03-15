444fb07f44b7d32ecdbb8c9c9183e9395e454a5b
Jonathan Feng <10607618+jonathanfeng@users.noreply.github.com>
2021-03-14 23:25:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const process = require('process');
+const fs = require('fs');
+const random = require('random');
 
+const client = new Discord.Client();
 // constants
 const DISCORD_API = process.env['DISCORD_API'];
 const PREFIX = '!';
+const simpleQuotes = JSON.parse(fs.readFileSync('quotes.json'));
+
+let lastPinger = '';
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  console.log(msg);
   // ignore these messages
   if (msg.author.bot) return;
   if (!msg.content.startsWith(PREFIX)) return
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  const commandBody = msg.content.slice(PREFIX.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  console.log(`commandBody = ${commandBody} args = ${args} command = ${command} lastPinger = ${lastPinger}`)
+
+  if (command === 'ping') {
+    if (msg.author.id === lastPinger) return;
+    msg.reply('lol fuck u bitch ass');
+    lastPinger = msg.author.id;
+  }
+
+  if (command === 'quote') {
+    const luckyNumber = random.int((min=0), (max=simpleQuotes.length));
+    msg.reply(`Sun Tzu says: "${simpleQuotes[luckyNumber]}"`);
   }
 });
 
